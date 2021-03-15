a46c88a3079ff06e3b76334976e8e120cded6240
Gleb-Rassada <79448263+Gleb-Rassada@users.noreply.github.com>
2021-03-15 13:24:41 +0300

    Add files via upload

---
+++ Lab_2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double CorrectNumber (string x) {
+    double n = 0;
+    while (true)
+    {
+        cout << x;
+        cin >> n;
+        if (!cin || n >= 1 || n <= -1)
+        {
+            cout << "Ошибка ввода!\n";
+            cin.clear();
+            while (cin.get() != '\n');
+        }
+        else break;
+    }
+    return n;
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+    double x,ln;
+    const int n = 10;
+    x = CorrectNumber("Выберите значение X из диапазона (0 ; 1): ");
+    for (int i = n; i > 0; i--)
+    {
+        ln = (pow(-1, i) * pow(x, 2 * i + 1)) / (2 * i + 1);
+    }
+    cout <<"Ответ: " << ln;
+
+    return 0;
+}
