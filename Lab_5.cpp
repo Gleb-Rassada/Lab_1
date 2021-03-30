789e3cd3dac51dccd843ffca92ef76d9029198d5
Gleb-Rassada <79448263+Gleb-Rassada@users.noreply.github.com>
2021-03-30 10:35:19 +0300

    Add files via upload

---
+++ Lab_5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    char str1[20];
+    char str2[20];
+    cout << "Enter a string:\n";
+    gets_s(str1); // VS ругается на gets();
+    int length1 = strlen(str1);
+    for (int i = 0, j = length1-1; i < length1; i++, j--) {
+        str2[i] = str1[j];
+    }
+    str2[length1] = '\0';
+    cout << "Inverted string:\n";
+    cout << str2;
+}
